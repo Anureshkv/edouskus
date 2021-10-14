cc85374c098d0dbd09d2f06c0083ea85dd3d6469
anuresh-eng <anureshkv1@gmail.com>
2021-10-13 21:21:12 -0700

    Updated

---
+++ docs/assets/vendor/bootstrap/js/readmore.js
@@ -0,0 +1,26 @@
+$(function(){
+    $(".read-more").readmore({
+
+        // text for Read Less button
+        hideText: "Read Less...",
+      
+        // text for Read More button
+        readText: "Read More...",
+      
+        // shows text on init
+        isTextShown: false,
+      
+        // enables fade animations
+        effect: true,
+      
+        // animation speed
+        effectOption: "fast",
+      
+        // button classes
+        buttonClasses: "btn-primary opacity-rollover",
+      
+        // button ID
+        id: "read-more-action"
+        
+      })
+    })
